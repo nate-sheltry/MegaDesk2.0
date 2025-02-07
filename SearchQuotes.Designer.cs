d372d70cc95e9cfcd8af82ed1515fa788a0a5a49
GetHelpPlz <104230407+GetHelpPlz@users.noreply.github.com>
2025-02-06 22:44:37 -0700

    number 2 ✓

--- SearchQuotes.Designer.cs
+++ SearchQuotes.Designer.cs
@@ -29,27 +29,49 @@
         private void InitializeComponent()
         {
             this.backToMenuBtn = new System.Windows.Forms.Button();
+            this.comboMaterial = new System.Windows.Forms.ComboBox();
+            this.gridQuotes = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridQuotes)).BeginInit();
             this.SuspendLayout();
             // 
             // backToMenuBtn
             // 
             this.backToMenuBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.backToMenuBtn.Location = new System.Drawing.Point(12, 258);
+            this.backToMenuBtn.Location = new System.Drawing.Point(12, 238);
             this.backToMenuBtn.Name = "backToMenuBtn";
-            this.backToMenuBtn.Size = new System.Drawing.Size(128, 37);
+            this.backToMenuBtn.Size = new System.Drawing.Size(128, 34);
             this.backToMenuBtn.TabIndex = 2;
             this.backToMenuBtn.Text = "&Back To Menu";
             this.backToMenuBtn.UseVisualStyleBackColor = true;
             this.backToMenuBtn.Click += new System.EventHandler(this.backToMenuBtn_Click);
             // 
+            // comboMaterial
+            // 
+            this.comboMaterial.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboMaterial.Location = new System.Drawing.Point(12, 12);
+            this.comboMaterial.Name = "comboMaterial";
+            this.comboMaterial.Size = new System.Drawing.Size(121, 20);
+            this.comboMaterial.TabIndex = 1;
+            this.comboMaterial.SelectedIndexChanged += new System.EventHandler(this.comboMaterial_SelectedIndexChanged);
+            // 
+            // gridQuotes
+            // 
+            this.gridQuotes.Location = new System.Drawing.Point(12, 52);
+            this.gridQuotes.Name = "gridQuotes";
+            this.gridQuotes.Size = new System.Drawing.Size(372, 180);
+            this.gridQuotes.TabIndex = 0;
+            // 
             // SearchQuotes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(396, 307);
+            this.ClientSize = new System.Drawing.Size(396, 283);
+            this.Controls.Add(this.gridQuotes);
+            this.Controls.Add(this.comboMaterial);
             this.Controls.Add(this.backToMenuBtn);
             this.Name = "SearchQuotes";
             this.Text = "MegaDesk - Nathaniel Dunham";
+            ((System.ComponentModel.ISupportInitialize)(this.gridQuotes)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,5 +79,7 @@
         #endregion
 
         private System.Windows.Forms.Button backToMenuBtn;
+        private System.Windows.Forms.ComboBox comboMaterial;
+        private System.Windows.Forms.DataGridView gridQuotes;
     }
 }
--- SearchQuotes.cs
+++ SearchQuotes.cs
@@ -7,14 +7,78 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace MegaDesk
 {
     public partial class SearchQuotes : Form
     {
+        private const string FILE_NAME = "quote.json"; // File where quotes are stored or txt file， I don't see where the quote is saved right now
+        private List<DeskQuote> allQuotes = new List<DeskQuote>();
+
         public SearchQuotes()
         {
             InitializeComponent();
+            LoadMaterials();
+            LoadQuotes();
+        }
+
+        // Load available materials
+        private void LoadMaterials()
+        {
+            comboMaterial.DataSource = Enum.GetValues(typeof(DesktopMaterial));
+            comboMaterial.SelectedIndex = -1;
+        }
+
+        private void LoadQuotes()
+        {
+            if (File.Exists(FILE_NAME))
+            {
+                try
+                {
+                    string json = File.ReadAllText(FILE_NAME);
+                    allQuotes = JsonConvert.DeserializeObject<List<DeskQuote>>(json);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading quotes: " + ex.Message);
+                }
+            }
+        }
+
+        // For selecting a material in the combo box
+        private void comboMaterial_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboMaterial.SelectedItem == null)
+                return;
+
+            DesktopMaterial selectedMaterial = (DesktopMaterial)comboMaterial.SelectedItem;
+
+            var filteredQuotes = allQuotes
+                .Where(q => q.material == selectedMaterial)
+                .Select(q => new
+                {
+                    Customer = q.customerName,
+                    Date = q.date.ToString("MM/dd/yyyy"),
+                    Width = q.width + " in.",
+                    Depth = q.depth + " in.",
+                    Drawers = q.drawers,
+                    Material = q.material.ToString(),
+                    OrderTime = q.order + " days",
+                    Price = "$" + q.price.ToString("F2")
+                })
+                .ToList();
+
+            // Display results
+            gridQuotes.DataSource = filteredQuotes;
         }
 
         private void backToMenuBtn_Click(object sender, EventArgs e)
